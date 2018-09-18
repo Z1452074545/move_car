f4a8d985a8c11c57d10a5f286d3f21cb9a4124ab
Z1452074545 <1452074545@qq.com>
2018-09-18 14:15:57 +0800

    move-car commit 1

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function() {
+    var img = ["imags/serve_car/menu1_1.png", "imags/serve_car/menu1_2.png",
+        "imags/serve_car/menu1_3.png", "imags/serve_car/menu1_4.png",
+        "imags/serve_car/menu1_5.png", "imags/serve_car/menu1_6.png",
+        "imags/serve_car/menu1_7.png", "imags/serve_car/menu1_8.png",
+    ]
+    var html = "";
+    for (var i = 0; i < img.length; i++) {
+        html += `<div class="img_box">
+             <a href="javascript:;">
+        <img src="${img[i]}" alt="" srcset="">
+        <p>道路救援</p>
+    </a>
+</div>`
+    }
+    $('.menu').append(html);
+})
